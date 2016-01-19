dfe1d10b28efe8004ad9ab68bdf3fea68c64e16f
yntema <gerrityntema@gmail.com>
2016-01-19 15:10:07 -0800

    complete grub class and bee subclass in es6

--- src/Bee.js
+++ src/Bee.js
@@ -1,7 +1,20 @@
 import Grub from './Grub'
 
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor () {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 };
 
 export default Bee;
+
+// Create a Bee class, in ES6 style, with:
+// the Grub superclass
+// an age property that is set to 5
+// a color property that is set to yellow
+// a food property that is inherited from grub
+// an eat method that is inherited from grub
+// a job property that is set to Keep on growing
--- src/Grub.js
+++ src/Grub.js
@@ -1,4 +1,19 @@
 class Grub {
+  constructor () {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+  eat () {
+    return 'Mmmmmmmmm jelly';
+  }
 };
 
 export default Grub;
+
+
+// Create a Grub class, in ES6 style, with:
+// an age property that is set to 0
+// a color property that is set to pink
+// a food property that is set to jelly
+// an eat method
